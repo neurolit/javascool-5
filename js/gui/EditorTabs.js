fbdd8d18f518bd08f362e2bcd1c23f7ac3e0e993
PhilippeGeek <PhilippeGeek@gmail.com>
2012-07-24 16:18:50 +0200

    Ammélioration du système d'onglets

--- js/gui/EditorTabs.js
+++ js/gui/EditorTabs.js
@@ -16,64 +16,62 @@ JVSEditorTabs.prototype = {
      * @type {jQuery}
      */
     $:null,
-    /**
-     * Le lastID est utilisé pour numéroter de façon incrémental les div.
-     */
-    lastID:0,
+
     openFile:function (fileUrl) {
+        var id=null;
         if (typeof fileUrl == "string") { // Open a file by url
             Console.log("Not implemented");
         } else { // Open a new File
 
             // Creation des IDs du nouvel onglet
-            var id = this.lastID++, htmlEditorId = "editorForFile-" + id, htmlTabId = "tabForFile-" + id;
+            //var id = this.lastID++, htmlEditorId = "editorForFile-" + id, htmlTabId = "tabForFile-" + id;
 
             // Création de l'instance du nouveau fichier et celle de l'editeur
             var file = new JVSFile();
             var editor = new JVSEditor();
 
-            // On les références dans les tableaux de l'objet
-            this.openedFiles[id] = file;
-            this.openedEditors[id] = editor;
+
 
             // Création de l'onglet
             var tabTitle = editor.title == undefined ? file.name : editor.title; // Le titre de l'onglet
-            this.$.children(".nav").append("<li class=\"\" id=\"" + htmlTabId + "\"><a href=\"#edit-file-" + id + "\" data-editid=\"" + id + "\" data-toggle=\"tab\">" + tabTitle + "<button class=\"close\">&times;</button></a></li>");
-            //$("#" + htmlTabId).tab("show");
-            $("#" + htmlTabId + " a").on('show', function (e) {
-                function idOfTab(domElement) {
-                    var id = parseInt($(domElement).data("editid"), 10);
-                    if (id == undefined)
-                        return -1;
-                    return id;
-                }
+            id=this.tabs.addTab(tabTitle,'The editor will go here',true);
 
-                var tab = $(e.target), id = idOfTab(e.target);
-                // On recalcule les IDs
-                var htmlEditorId = "editorForFile-" + id, oldHtmlEditorId = "editorForFile-" + idOfTab(e.relatedTarget);
-
-                // Supprime les éditeurs visibles
-                $("#" + oldHtmlEditorId).removeClass("active").trigger("hidden");
-
-                // Affiche l'editeur voulut
-                $("#" + htmlEditorId).addClass("active").trigger("visible");
-
-            });
-
-            // On ajoute la div de l'éditeur
-            this.$.children(".tab-content").append("<div class=\"tab-pane\" id=\"" + htmlEditorId + "\"></div>");
+            // On les références dans les tableaux de l'objet
+            this.openedFiles[id] = file;
+            this.openedEditors[id] = editor;
+//            this.$.children(".nav").append("<li class=\"\" id=\"" + htmlTabId + "\"><a href=\"#edit-file-" + id + "\" data-editid=\"" + id + "\" data-toggle=\"tab\">" + tabTitle + "<button class=\"close\">&times;</button></a></li>");
+//            $("#" + htmlTabId).tab("show");
+//            $("#" + htmlTabId + " a").on('show', function (e) {
+//                function idOfTab(domElement) {
+//                    var id = parseInt($(domElement).data("editid"), 10);
+//                    if (id == undefined)
+//                        return -1;
+//                    return id;
+//                }
+//
+//                var tab = $(e.target), id = idOfTab(e.target);
+//                // On recalcule les IDs
+//                var htmlEditorId = "editorForFile-" + id, oldHtmlEditorId = "editorForFile-" + idOfTab(e.relatedTarget);
+//
+//                // Supprime les éditeurs visibles
+//                $("#" + oldHtmlEditorId).removeClass("active").trigger("hidden");
+//
+//                // Affiche l'editeur voulut
+//                $("#" + htmlEditorId).addClass("active").trigger("visible");
+//
+//            });
+//
+//            // On ajoute la div de l'éditeur
+//            this.$.children(".tab-content").append("<div class=\"tab-pane\" id=\"" + htmlEditorId + "\"></div>");
 
             // On passe la main à l'éditeur pour sa construction
-            editor.setup(file, htmlEditorId, id);
+//            editor.setup(file, htmlEditorId, id);
 
             // On supprime le message dissant qu'aucun fichier n'est ouvert
             this.hideNoFileMessage();
 
-            // On ouvre l'onglet créé
-            setTimeout(function () {
-                $("#" + htmlTabId + " a").trigger("click")
-            }, 100);
         }
+        return id;
 
     },
     closeFile:function (file) {
@@ -96,14 +94,14 @@ JVSEditorTabs.prototype = {
         // On simplifie l'accesseur JQuery
         this.$ = $("#" + this.htmlID);
 
-
+        // On initialise le système d'onglets
         this.tabs = new JVSTabs(this.$[0]);
 
         // On lance la configuration des écouteurs sur ce gestionnaire
         this.setupListenersOnEditorTabs();
 
         // On affiche à l'utilisateur qu'il est censé ouvrir un fichier
-        this.showNoFileMessage();
+//        this.showNoFileMessage();
     },
     showNoFileMessage:function () {
         this.assertIfAmIOnScreen();
--- js/gui/Tabs.js
+++ js/gui/Tabs.js
@@ -5,17 +5,68 @@
  */
 function JVSTabs(domElem) {
     this.$ = $(domElem);
-    this.$.addClass("jvstable");
-    this.$.html('<ul class="nav nav-tabs"></ul><div class="tab-content"></div>');
+    this.id=Math.uuid(5,16);
+    this.$.addClass("jvstabs");
+    this.$.html('<ul class="nav nav-pills"></ul><div class="tab-content"></div>');
 }
 JVSTabs.prototype = {
-    addTab:function (title, content) {
-
+    /**
+     * Le lastID est utilisé pour numéroter de façon incrémental les div.
+     */
+    lastID:0,
+    idOfTabShown:null,
+    addTab:function (title, content, donotshow) {
+        title=title||"Onglet";
+        content=content||"";
+        donotshow=donotshow==null?false:donotshow;
+        var id=this.lastID++;
+        this.$.children('.nav').append('<li id="'+this.idForTab(id)+'"><a class="link">'+title+'</a><span></span></li>');
+        $('#'+this.idForTab(id)+' a').click({tabs:this,id:id},function(e){
+            e.data.tabs.showTab(e.data.id);
+        });
+        this.$.children('.tab-content').append('<div id="'+this.idForContent(id)+'" class="tab-pane">'+content+'</div>');
+        donotshow?null:this.showTab(id);
+        return id;
     },
     removeTab:function (id) {
+        // On prévient les deux enfant que l'on ferme
+        var event = jQuery.Event("closing");
+        $('#'+this.idForTab(id)).triggerHandler(event);
+        $('#'+this.idForContent(id)).triggerHandler(event);
+        // Todo: Annuler la fermeture si la propagation a été arrêté de force
+//        if ( event.isDefaultPrevented() ) { // S'ils sont tous d'accord, alors on ferme
+            // On ferme
+            $('#'+this.idForTab(id)+', #'+this.idForContent(id)).remove();
+//        }
 
+    },
+    showTab:function(id){
+        if(this.idOfTabShown!=null){
+            $('#'+this.idForTab(this.idOfTabShown)+', #'+this.idForContent(this.idOfTabShown)).removeClass('active');
+        }
+        $('#'+this.idForTab(id)+', #'+this.idForContent(id)).addClass('active');
+        this.idOfTabShown=id;
     },
     count:function () {
         return this.$.children(".nav-tabs").children().length();
+    },
+    idFor:function(element,id){
+        return this.id+'-'+element+'-'+id
+    },
+    idForTab:function(id){
+        return this.idFor('tab',id);
+    },
+    idForContent:function(id){
+        if(id==null)id=this.lastID;
+        return this.idFor('content',id);
+    },
+    manageOverflow:function(){
+        var tab=this.$.children('.nav').children('li'),actualWidthOfTab=$(this.$.children('.nav')).width(),neededWidth=0;
+        for(li in tab.toArray()){
+            neededWidth+=$(tab[li]).outerWidth(true);
+        }
+        if(actualWidthOfTab<neededWidth){
+
+        }
     }
 };
