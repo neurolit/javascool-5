f67892efd427c7aff46754ae9c4577cc5ac6b279
PhilippeGeek <PhilippeGeek@gmail.com>
2012-08-17 14:59:11 +0200

    No message

--- js/ls2var.sh
+++ js/ls2var.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 cd ./js
-ls=`ls -1 [A-Z]*.js */[A-Z]*.js | sed 's/[\/]*\///' | sed 's/\.js$//' | sed 's/\(.*\)/"\1",/'`
+ls=`ls -1 [A-Z]*.js */[A-Z]*.js | sed 's/\.js$//' | sed 's/\(.*\)/"\1",/'`
 echo $ls | sed 's/,$//'
-
+cd ..
+grep '^%files%.*' index.html
 
 
