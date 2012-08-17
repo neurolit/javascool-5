e169cc982dcf972e6f8398e55dbb7d5e8c03dd0b
vthierry <thierry.vieville@inria.fr>
2012-08-17 15:12:29 +0200

    committed from makefile

--- js/ls2var.sh
+++ js/ls2var.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cd ./js
-ls=`ls -1 [A-Z]*.js */[A-Z]*.js | sed 's/\.js$//' | sed 's/\(.*\)/"\1",/'`
-echo $ls | sed 's/,$//'
-cd ..
-grep '^%files%.*' index.html
+ls=`ls -1 [A-Z]*.js */[A-Z]*.js | sed 's/.*\///' | sed 's/\.js$//' | sed 's/\(.*\)/"\1",/'`
+ls=`echo $ls | sed 's/,$//'`
+cp ../index.html ../index.html~ ; sed "s/.*\/\*%files%\*\/.*/\/*%files%*\/ `echo $ls | sed 's/\"/\\\\\"/g'`/" < ../index.html~ > ../index.html 
 
-
+grep %files% ../index.html
