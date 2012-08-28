0b7ecc2815531e3e1b4ff9d6c61f0def5ec73474
PhilippeGeek <PhilippeGeek@gmail.com>
2012-08-28 14:11:11 +0200

    Bug du focus résolu

--- js/ProgletApplet.js
+++ js/ProgletApplet.js
@@ -13,8 +13,10 @@ javascool.ProgletApplet=function(proglet) {
 //    /if(javascool.ProgletApplet.model==undefined)
         javascool.ProgletApplet.model=javascool.PolyFileWriter.load(javascool.location+"/js/ProgletApplet.html");
     this.openUserProgram=function(program){
-        if(javascool.ProgletApplet.openedPopup.closed==false)
+        if(javascool.ProgletApplet.openedPopup.closed==false){
             javascool.ProgletApplet.openedPopup.focus();
+            return;
+        }
         var code=javascool.ProgletApplet.model;
         code=code.replace("@w",400).replace("@h",600).replace("@jars", proglet.hasJar?", proglets/"+proglet.namespace+"/"+proglet.namespace+".jar":"").replace(/@id/g,"ProgPane");
         code=code.replace("@label",proglet.title).replace("@proglet",proglet.hasPanel?"org.javascool.proglets."+proglet.namespace+".Panel":"").replace("@runnable",program);
